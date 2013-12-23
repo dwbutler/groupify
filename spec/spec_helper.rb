8f372c0fd17a84c18415ab0313df6fd994a06ea4
David Butler <dwbutler@ucla.edu>
2013-12-23 13:02:00 -0800

    Test with Mongoid 4.0.0.alpha1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ Bundler.require(:default, :test, :development)
 
 JRUBY = defined?(JRUBY_VERSION)
 
+puts "Mongoid version #{Mongoid::VERSION}"
+puts "ActiveRecord version #{ActiveSupport::VERSION}"
+
 RSpec.configure do |config|
   config.order = "random"
 end
