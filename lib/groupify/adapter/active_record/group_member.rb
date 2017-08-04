ed4337029aae3cbbd5998747d115226753c4bc84
Joel Van Horn <joel@joelvanhorn.com>
2017-08-04 18:47:19 -0400

    Add proper default class

--- lib/groupify/adapter/active_record/group_member.rb
+++ lib/groupify/adapter/active_record/group_member.rb
@@ -21,7 +21,7 @@ module Groupify
           dependent: :destroy,
           class_name: Groupify.group_membership_class_name
 
-        has_group :groups
+        has_group :groups, class_name: @group_class_name
       end
 
       def in_group?(group, opts = {})
