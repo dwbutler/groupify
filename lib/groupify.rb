691a28d26390cec37f77902176ee9d4ea930b337
Joel Van Horn <joel@joelvanhorn.com>
2017-08-05 02:05:35 -0400

    Add option to return a string version of inferred class name rather than trying to constantize

--- lib/groupify.rb
+++ lib/groupify.rb
@@ -3,11 +3,13 @@ require 'active_support'
 module Groupify
   mattr_accessor :group_membership_class_name,
                  :group_class_name,
-                 :ignore_base_class_inference_errors
+                 :ignore_base_class_inference_errors,
+                 :ignore_association_class_inference_errors
 
   self.group_class_name = 'Group'
   self.group_membership_class_name = 'GroupMembership'
   self.ignore_base_class_inference_errors = true
+  self.ignore_association_class_inference_errors = true
 
   def self.configure
     yield self
@@ -20,12 +22,16 @@ module Groupify
   def self.infer_class_and_association_name(association_name)
     begin
       klass = association_name.to_s.classify.constantize
-    rescue StandardError => ex
+    rescue NameError => ex
       puts "Error: #{ex.inspect}"
       #puts ex.backtrace
+
+      if Groupify.ignore_association_class_inference_errors
+        klass = association_name.to_s.classify
+      end
     end
 
-    if !association_name.is_a?(Symbol) && klass
+    if !association_name.is_a?(Symbol) && klass.is_a?(Class)
       association_name = klass.model_name.plural
     end
 
