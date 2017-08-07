09cadcb2cfd2c5b8a5225e49ba8e5670fc3bbdd9
Joel Van Horn <joel@joelvanhorn.com>
2017-08-07 00:02:37 -0400

    Simplify class

--- lib/groupify/adapter/active_record/association_extensions.rb
+++ lib/groupify/adapter/active_record/association_extensions.rb
@@ -10,24 +10,20 @@ module Groupify
       end
 
       def parent_proxy
-        @parent_proxy ||= ParentProxy.new(proxy_association.owner, parent_type)
+        @parent_proxy ||= ParentProxy.new(proxy_association.owner, proxy_association.through_reflection.name.match(/_(group|member)$/)[1].to_sym)
       end
 
     protected
 
+      # Throw an exception here when adding direction to an association
+      # because when adding the children to the parent this won't
+      # happen because the group membership is polymorphic.
       def add_children(children, options = {})
-        # Throw an exception here when adding direction to an association
-        # because when adding the children to the parent this won't
-        # happen because the group membership is polymorphic.
         children.each do |child|
           proxy_association.__send__(:raise_on_type_mismatch!, child)
         end
 
         super
-      end
-
-      def parent_type
-        @parent_type ||= proxy_association.through_reflection.name == :group_memberships_as_group ? :group : :member
       end
     end
   end
