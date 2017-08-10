2b82b3b5aafdec28dd9e0c9832b3059f801eaf0a
Joel Van Horn <joel@joelvanhorn.com>
2017-08-10 16:37:51 -0400

    Don't delegate methods to create records

--- lib/groupify/adapter/active_record/polymorphic_relation.rb
+++ lib/groupify/adapter/active_record/polymorphic_relation.rb
@@ -5,7 +5,7 @@ module Groupify
 
       def initialize(parent_proxy, &group_membership_filter)
         @parent_proxy = parent_proxy
-        
+
         super(parent_proxy.child_type) do
           query = merge(parent_proxy.memberships_association)
           query = query.instance_eval(&group_membership_filter) if block_given?
@@ -18,11 +18,6 @@ module Groupify
 
         self
       end
-
-      # When trying to create a new record for this collection,
-      # create it on the `member.default_groups` or `group.default_members`
-      # association.
-      def_delegators :default_association, :build, :create, :create!
 
       attr_reader :collection, :parent_proxy
 
