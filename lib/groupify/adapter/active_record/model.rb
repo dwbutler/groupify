434b6e6f317128156007209ee326c744aef690d4
Joel Van Horn <joel@joelvanhorn.com>
2017-08-10 22:19:30 -0400

    Revert ActiveRecord configuration to inline for clarity

--- lib/groupify/adapter/active_record/model.rb
+++ lib/groupify/adapter/active_record/model.rb
@@ -21,13 +21,54 @@ module Groupify
         def acts_as_group(opts = {})
           include Groupify::ActiveRecord::Group
 
-          ParentProxy.configure(self, :group, opts)
+          # Get defaults from parent class for STI
+          self.default_member_class_name = Groupify.superclass_fetch(self, :default_member_class_name, Groupify.member_class_name)
+          self.default_members_association_name = Groupify.superclass_fetch(self, :default_members_association_name, Groupify.members_association_name)
+
+          if (member_association_names = opts.delete :members)
+            has_members(member_association_names)
+          end
+
+          if (default_members = opts.delete :default_members)
+            self.default_member_class_name = default_members.to_s.classify
+            # Only use as the association name if none specified (backwards-compatibility)
+            self.default_members_association_name ||= default_members
+          end
+
+          if default_members_association_name
+            has_member(default_members_association_name,
+              source_type: ActiveRecord.base_class_name(default_member_class_name),
+              class_name: default_member_class_name
+            )
+          end
         end
 
         def acts_as_group_member(opts = {})
           include Groupify::ActiveRecord::GroupMember
 
-          ParentProxy.configure(self, :member, opts)
+          # Get defaults from parent class for STI
+          self.default_group_class_name = Groupify.superclass_fetch(self, :default_group_class_name, Groupify.group_class_name)
+          self.default_groups_association_name = Groupify.superclass_fetch(self, :default_groups_association_name, Groupify.groups_association_name)
+
+          if (group_association_names = opts.delete :groups)
+            has_groups(group_association_names)
+          end
+
+          if (default_groups = opts.delete :default_groups)
+            self.default_group_class_name = default_groups.to_s.classify
+            self.default_groups_association_name ||= default_groups
+          end
+
+          # Deprecated: for backwards-compatibility
+          if (group_class_name = opts.delete :group_class_name)
+            self.default_group_class_name = group_class_name
+          end
+
+          if default_groups_association_name
+            has_group default_groups_association_name,
+              source_type: ActiveRecord.base_class_name(default_group_class_name),
+              class_name: default_group_class_name
+          end
         end
 
         def acts_as_named_group_member(opts = {})
