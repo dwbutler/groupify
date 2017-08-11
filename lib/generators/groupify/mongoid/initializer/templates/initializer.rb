d9305e26622146def672874e9ed30c8f1f4477c9
Joel Van Horn <joel@joelvanhorn.com>
2017-08-11 01:11:11 -0400

    Update generator with default configuration comments

--- lib/generators/groupify/mongoid/initializer/templates/initializer.rb
+++ lib/generators/groupify/mongoid/initializer/templates/initializer.rb
@@ -1,5 +1,15 @@
 Groupify.configure do |config|
-  # Configure the default group class name.
-  # Defaults to 'Group'
+  # Configure the default group and member class names.
+  # Default to `nil`
   # config.group_class_name = 'Group'
+  # config.member_class_name = 'User'
+
+  # Configure the default group membership class name.
+  # Defaults to 'GroupMembership'
+  # config.group_membership_class_name = 'GroupMembership'
+
+  # Configure the default group and member association names.
+  # Default to `false` - specify names to auto-create them
+  # config.groups_association_name = :groups
+  # config.members_association_name = :members
 end
